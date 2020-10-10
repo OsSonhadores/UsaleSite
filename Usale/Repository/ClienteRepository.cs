9161fecff675b59e470a100047191380eae30619
= <=>
2020-10-10 19:02:08 -0300

    so pra subir

---
+++ Usale/Repository/ClienteRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Usale.Repository
+{
+    public interface IClienteRepository
+    {
+        void Logar(string fodase);
+    }
+
+    public class ClienteRepository : IClienteRepository
+    {
+
+        public void Logar(string fodase)
+        {
+
+        }
+
+    }
+}
---
+++ Usale/Service/UserService .cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Usale.DTO;
+
+namespace Usale.Service
+{
+    public interface IUserService
+    {
+        Task CadastroCliente(ClienteDTO c);
+        Task CadastroVendedor(VendedorDTO v);
+    }
+
+    public class UserService : IUserService
+    {
+
+        private readonly IUserRepository _rep;
+
+
+        public async Task CadastroVendedor(VendedorDTO v)
+        {
+            await _rep.CadastroVendedor(vEntity);
+        }
+
+        public async Task CadastroCliente(ClienteDTO c)
+        {
+            await _rep.CadastroCliente(cEntity);
+        }
+
+
+
+    }
+}
