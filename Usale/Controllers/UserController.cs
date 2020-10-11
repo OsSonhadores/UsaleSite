67126c547cbb3b8cd4404d0505b3a343adf3932c
Lucas Lima <lucaslim.lux@gmail.com>
2020-10-11 01:30:44 -0300

    Uploading font-awesome and some changes

--- Usale/Controllers/UserController.cs
+++ Usale/Controllers/UserController.cs
@@ -18,16 +18,26 @@ namespace Usale.Controllers
             _serv = serv;
         }
 
+        public IActionResult UserCliente()
+        {
+            return View();
+        }
+
+        public IActionResult UserLojista()
+        {
+            return View();
+        }
+
         [HttpGet]
         public async Task CadastroVendedor([FromForm] VendedorDTO v)
         {
-            await _serv.CadastroVendedor(v);
+            //await _serv.CadastroVendedor(v);
         }
 
         [HttpGet]
         public async Task CadastroCliente([FromForm] ClienteDTO c)
         {
-            await _serv.CadastroCliente(c);
+            //await _serv.CadastroCliente(c);
         }
 
     }
